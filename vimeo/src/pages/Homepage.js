ade93a5ab7b8c22de115d59acec3254ed039598b
Leman-Y <Leman.Yan80@myhunter.cuny.edu>
2019-11-25 21:56:42 -0500

    Done with llayout

--- vimeo/src/pages/Homepage.js
+++ vimeo/src/pages/Homepage.js
@@ -7,34 +7,36 @@ class Homepage extends Component{
 		return(
     <div className="Container">
       <div className="Monsoon">
-        <div className="Item">
-          <div>
+        <div className="Monsoon-item">
+          <div className='imgs'>
           	<img src='https://i.vimeocdn.com/video/595198868_505x160.jpg' alt='Monsoon III'/>
           </div>
-          <div className='Text-box'>
+          <div className='Monsoon-text-box'>
           	<h1>MONSOON III</h1>
           	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed commodo diam. Nullam iaculis commodo tellus, eu consectetur turpis fringilla in. Proin vitae mollis odio, in malesuada nisi. Nulla id dolor quis nisl lacinia aliquam sed ullamcorper nisl. Donec in mauris felis.</p>
           </div>
         </div>
       </div>
       <div className='Beams'>
-        <div className='Item'>
-            <div  className='Beams-text-box'>
+        <div className='Beams-item'>
+          <div className='Beams-text-box'>
         		  <h1>BEAMS</h1>
         		  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed commodo diam. Nullam iaculis commodo tellus, eu consectetur turpis fringilla in. Proin vitae mollis odio, in malesuada nisi. Nulla id dolor quis nisl lacinia aliquam sed ullamcorper nisl. Donec in mauris felis.</p>
-            </div> 
-        	<div>
+          </div> 
+        	<div className='imgs'>
         		<img src='https://i.vimeocdn.com/video/589972810_530x315.jpg' alt='Beams'/>
         	</div>
         </div>
       </div>
       <div className='Move-2'>
-      	<div className='Item'>
-      		<img src='https://i.vimeocdn.com/video/590587169_530x315.jpg' alt='Move 2'/>
-      	</div>
-        <div>
-          <h1>Move 2</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed commodo diam. Nullam iaculis commodo tellus, eu consectetur turpis fringilla in. Proin vitae mollis odio, in malesuada nisi. Nulla id dolor quis nisl lacinia aliquam sed ullamcorper nisl. Donec in mauris felis.</p>
+      	<div className='Move-2-item'>
+          <div className='imgs'>
+        		<img src='https://i.vimeocdn.com/video/590587169_530x315.jpg' alt='Move 2'/>
+        	</div>
+          <div className='Move-2-text-box'>
+            <h1>Move 2</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed commodo diam. Nullam iaculis commodo tellus, eu consectetur turpis fringilla in. Proin vitae mollis odio, in malesuada nisi. Nulla id dolor quis nisl lacinia aliquam sed ullamcorper nisl. Donec in mauris felis.</p>
+          </div>
         </div>
       </div>
     </div>
