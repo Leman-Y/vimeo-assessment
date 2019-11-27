c4372c129d57131815d2a53ed77b19a75d004752
Leman-Y <Leman.Yan80@myhunter.cuny.edu>
2019-11-27 10:00:06 -0500

    Cleaned up comments

--- vimeo/src/components/Carousel.js
+++ vimeo/src/components/Carousel.js
@@ -1,15 +1,6 @@
 import React, {Component} from 'react';
 import './Carousel.css';
 
-const imgUrls = [
-	'https://m.media-amazon.com/images/M/MV5BMjI1MDQ2MDg5Ml5BMl5BanBnXkFtZTgwMjc2NjM5ODE@._V1_UX182_CR0,0,182,268_AL_.jpg',
-	'https://m.media-amazon.com/images/M/MV5BMTUwMTQ1NDQ2M15BMl5BanBnXkFtZTgwMDU1MTc4NjE@._V1_UY268_CR3,0,182,268_AL_.jpg',
-	'https://images.squarespace-cdn.com/content/v1/59e6208ae45a7cbaeb91a7fc/1508866806198-6H806R2SQKYSTGQ11BOI/ke17ZwdGBToddI8pDm48kG6DEqWFQGN86Yy_kIwYHV57gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmQPoRzxSr1hzN-vPBHt7YyK04k3j-IK39m3MLCy1OWzJvtn8WJ5C3AsqSwDQDxoEq/image-asset.jpeg',
-	'http://www.gstatic.com/tv/thumb/v22vodart/12392207/p12392207_v_v8_aa.jpg',
-	'https://musicimage.xboxlive.com/catalog/video.movie.8D6KGX0RV3GC/image?locale=en-us&mode=crop&purposes=BoxArt&q=90&h=225&w=150&format=jpg'
-
-];
-
 const items = [
 	{
 		src: 'https://m.media-amazon.com/images/M/MV5BMjI1MDQ2MDg5Ml5BMl5BanBnXkFtZTgwMjc2NjM5ODE@._V1_UX182_CR0,0,182,268_AL_.jpg',
@@ -19,7 +10,7 @@ const items = [
 	},
 	{
 		src: 'https://m.media-amazon.com/images/M/MV5BMTUwMTQ1NDQ2M15BMl5BanBnXkFtZTgwMDU1MTc4NjE@._V1_UY268_CR3,0,182,268_AL_.jpg',
-		altText: 'A Decent Man',
+		altText: 'Nichts Passiert',
 		caption: 'Thomas Engel is always anxious to avoid conflict. At any cost. This compulsive striving for harmony, however, proves to be his road to ruin. He will fight for a peaceful solution. Violently, if need be.',
 		linearGradient: ''
 	},
@@ -58,7 +49,7 @@ class Carousel extends Component {
 	}
 	
 	previousSlide () {
-		const lastIndex = imgUrls.length - 1;
+		const lastIndex = items.length - 1;
 		const { currentImageIndex } = this.state;
 		const shouldResetIndex = currentImageIndex === 0;
 		const index =  shouldResetIndex ? lastIndex : currentImageIndex - 1;
@@ -69,7 +60,7 @@ class Carousel extends Component {
 	}
 	
 	nextSlide () {
-		const lastIndex = imgUrls.length - 1;
+		const lastIndex = items.length - 1;
 		const { currentImageIndex } = this.state;
 		const shouldResetIndex = currentImageIndex === lastIndex;
 		const index =  shouldResetIndex ? 0 : currentImageIndex + 1;
@@ -78,24 +69,6 @@ class Carousel extends Component {
 			currentImageIndex: index
 		});
 	}
-	
-//				<img className='backgroundImgCont' src={imgUrls[this.state.currentImageIndex]} />
-//				<ImageSlide url={ imgUrls[this.state.currentImageIndex] } />
-/*
-				<img className='Background' src={imgUrls[this.state.currentImageIndex]} />
-				<div className='card-text-container'>
-					<img className='Card' src={imgUrls[this.state.currentImageIndex]} />
-				</div>
-				<Arrow direction="left" clickFunction={ this.previousSlide } glyph="&#9664;" />
-				<Arrow direction="right" clickFunction={ this.nextSlide } glyph="&#9654;" />
-				<div className='Background'>
-
-				</div>
-				<ImageSlide url={ imgUrls[this.state.currentImageIndex] } />
-				<ImageSlide url={ items[this.state.currentImageIndex].src } />
-				<img className='Card' src={url} />
-
-*/
 
 	render () {
 		return (
@@ -129,19 +102,11 @@ const Arrow = ({ direction, clickFunction, glyph }) => (
 const ImageSlide = ({ url, linearGradient }) => {
 	const styles = {
 		/*backgroundImage: `url(${url})`, ${linearGradient},*/
-  		/*background: `url(${url})`, */
+		/*background: `linear-gradient(0deg, rgba(0,0,139 ,0.1 ), rgba(0,0,139 , 0.1 )), url(${url}) `,  */
 		backgroundImage: `url(${url})`,
-	/*	background: `linear-gradient(0deg, rgba(0,0,139 ,0.1 ), rgba(0,0,139 , 0.1 )), url(${url}) `,  */
 		backgroundSize: 'cover',
 		height: '500px',
 		backgroundPosition: 'center',
-	/*
-		background: 'linear-gradient(rgba(30,144,255,0.1), rgba(30,144,255,0.1))'
-		backgroundSize: 'cover',
-		backgroundPosition: 'center',
-		height: '500px',
-		filter: 'blur(50px)'
-	*/
 	};
 
 	
